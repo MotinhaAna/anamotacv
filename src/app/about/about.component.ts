ba12b7e65407f9e10b79b5a89f7de24cbdc8a75e
anamota <ana.mota@checkmarx.com>
2021-01-17 23:15:34 +0000

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+
+  constructor() { }
+
+  itemImageUrl = '../assets/images/avatar_am.png';
+
+  aboutme = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to";
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { SectionsComponent } from './sections/sections.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    NavigatorComponent,
+    SectionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
